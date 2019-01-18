04a577472f6b564d3946c81766e991c5edf37390
Enzo <dorosario.enzo@protonmail.com>
2019-01-18 09:11:55 +0100

    add subtitle

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-var express = require("express");
-var path = require("path");
+var express = require('express');
+var path = require('path');
 var app = express();
-var hcf = require("./hcf");
 
-var http = require("http").Server(app);
-var io = require("socket.io")(http);
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.use(express.static("./"));
+app.use(express.static('./'));
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname + "/index.html"));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-io.on("connection", function(socket) {
-  socket.on("command enter", function(command) {
-    if (command === "clear") {
-      socket.emit("clear");
-    } else if (command === "help") {
-      socket.emit("help");
-    } else if (command === "hcf") {
-      socket.emit("hcf");
-    } else {
-      socket.emit("unknown");
-    }
-  });
+io.on('connection', function(socket) {
+    socket.on('command enter', function(command) {
+        if (command === 'clear') {
+            socket.emit('clear');
+        } else if (command === 'help') {
+            socket.emit('help');
+        } else if (command === 'enzo') {
+            socket.emit('enzo');
+        } else {
+            socket.emit('unknown');
+        }
+    });
 });
 
 http.listen(5050, function() {
-  console.log("Listening on port 5050...");
+    console.log('Listening on port 5050...');
 });
